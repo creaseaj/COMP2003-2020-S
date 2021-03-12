628163de6a993af13840ebfc8ab7cc1fc23490a1
charlie-grayson <57486129+charlie-grayson@users.noreply.github.com>
2021-03-12 15:48:58 +0000

    Update nmapController.cs
    Added function to obtain IP Address

--- VAPS - Software/VAPS/Controller/nmapController.cs
+++ VAPS - Software/VAPS/Controller/nmapController.cs
@@ -122,6 +122,19 @@ namespace VAPS.Controller
                 return "";
             }
         }
+        
+        public string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
 
     }
 }
